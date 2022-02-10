04ac4db5e26a84ffe1c3be3f5b6635a9fe399e99
Yash-Tiwari24 <yash.t@thinkbridge.in>
2022-02-10 09:24:27 +0530

    Added ViewData

--- Controller/HomeController.cs
+++ Controller/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Book_Store.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,19 +9,32 @@ namespace Book_Store.Controllers
 {
     public class HomeController:Controller
     {
+        [ViewData]
+        public string CustomProperty { get; set; }
+
+        [ViewData]
+        public string Title { get; set; }
+        
+      
+
+
         public ViewResult Index()
         {
-           
+            CustomProperty = "Custom Value";
+            Title = "Home Page From Controller";
+           //ViewBag.Title = 123;
             return View();
         }
 
         public ViewResult AboutUs()
         {
+            Title = "AboutUs Page From Controller";
             return View();
         }
 
         public ViewResult ContactUs()
         {
+            Title = "ContactUs Page From Controller";
             return View();
         }
 
