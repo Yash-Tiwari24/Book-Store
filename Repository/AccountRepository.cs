995165e10cbd2053506e18c771b8736111d57717
Yash-Tiwari24 <yash.t@thinkbridge.in>
2022-02-13 22:24:17 +0530

    log out in asp.net core

--- Repository/AccountRepository.cs
+++ Repository/AccountRepository.cs
@@ -10,10 +10,12 @@ namespace Book_Store.Repository
     public class AccountRepository : IAccountRepository
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountRepository(UserManager<ApplicationUser> userManager)
+        public AccountRepository(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
         public async Task<IdentityResult> CreateUserAsync(SignUpUserModel userModel)
         {
@@ -28,6 +30,16 @@ namespace Book_Store.Repository
             };
          var result=  await _userManager.CreateAsync(user, userModel.Password);
             return result;
+        }
+
+        public async Task<SignInResult> PasswordSignInAsync(SignInModel signInModel )
+        {
+            return  await _signInManager.PasswordSignInAsync(signInModel.Email,signInModel.Password,signInModel.RememberMe,false);
+        }
+
+        public async Task SignOutAsync()
+        {
+            await _signInManager.SignOutAsync();
         }
     }
 }
--- Repository/IAccountRepository.cs
+++ Repository/IAccountRepository.cs
@@ -7,5 +7,11 @@ namespace Book_Store.Repository
     public interface IAccountRepository
     {
         Task<IdentityResult> CreateUserAsync(SignUpUserModel userModel);
+
+        Task<SignInResult> PasswordSignInAsync(SignInModel signInModel);
+
+        Task SignOutAsync();
+
+
     }
 }
